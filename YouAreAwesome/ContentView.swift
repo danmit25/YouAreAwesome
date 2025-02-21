495c11ea2481cbe6e420e9bd03bca3a040b0b8db
Dan Mit <danmit25@icloud.com>
2025-02-21 01:11:28 +0100

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,29 @@
+ //
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Dan Mit on 20.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text("You Are Awesome!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+//                .fontDesign(.serif)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
